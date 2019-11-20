14d06ad9491f586ea2f916ec17ce6ff6936f9c20
Your Name <kateryna.skliaruk6@gmail.com>
2019-11-20 13:30:29 +0100

    Updates

---
+++ precache-manifest.46af2808641692ab3a199944b4b4113b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85318d92445a4332bdeb2b8d12a7c47f",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "fbaeda7a7e58fe25d0b4",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f651092c5e7b7a8b9471",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.78b926ae.chunk.css"
+  },
+  {
+    "revision": "fbaeda7a7e58fe25d0b4",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.b96960f7.chunk.js"
+  },
+  {
+    "revision": "f651092c5e7b7a8b9471",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.7ab52daf.chunk.js"
+  },
+  {
+    "revision": "0c18147979c7c79bba04",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.eaa3779c.js"
+  }
+]);
